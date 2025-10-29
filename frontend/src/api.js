d0f83c0fc8a47f779d9294e4f5d4cf43a6bd2dec
Vinit Raj Soni <131142169+vinitrajsoni@users.noreply.github.com>
2025-10-29 17:47:02 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/", // your backend endpoint
+  baseURL: "https://realstatechatbot.onrender.com", // your backend endpoint
 });
 
 export default api;
+
